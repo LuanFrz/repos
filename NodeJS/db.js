9fcc13cde84dd47149c934a51eec2ec8a1e4e318
LuanFrz <luan.frizzi@iv2.com.br>
2019-12-04 10:57:26 -0300

    Project in working state.

--- NodeJS/db.js
+++
@@ -1,5 +0,0 @@
-// db.js purpose is to specify the connection to the database.
-
-const mongoose = require('mongoose');
-
-mongoose.connect('mongodb+srv://NodeJS-API:potastic@cluster-nep6i.gcp.mongodb.net/test?retryWrites=true&w=majority');
--- NodeJS/server.js
+++
@@ -1,8 +0,0 @@
-// server.js purpose is to activate the NodeJS server on a specified port.
-
-var app = require('./app');
-var port = process.env.PORT || 3000;
-
-app.listen(port, function(){
-    console.log('Server running on port ' + port);
-});
