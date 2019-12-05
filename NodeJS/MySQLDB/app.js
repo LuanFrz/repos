33350287db03fe1ad051526831264736860e7a32
LuanFrz <luan.frizzi@iv2.com.br>
2019-12-05 14:29:54 -0300

    I dunno what changed, just training...

--- NodeJS/MySQLDB/app.js
+++ NodeJS/MySQLDB/app.js
@@ -4,77 +4,22 @@ const mysql = require('mysql');
 const bodyParser = require('body-parser');
 const app = express();
 
-function getConnection () {
-    return mysql.createConnection({
-        host: 'localhost',
-        user: 'root',
-        database: 'MySQL-API'
-    });
-}
+const router = require('./routes/user.js')
+
+app.use(router);
 
 var urlencodedParser = bodyParser.urlencoded({ extended: false });
 app.use(urlencodedParser);
 app.use(bodyParser.json());
 
-// Makes it so the app serves the files on /public.
+// Makes it so the app serves the files located in /public.
 app.use(express.static('./public'));
 
 app.use(morgan('short'));
 
-app.post('/user_create', (req, res) => {
-
-    const firstName = req.body.createFirstName;
-    const lastName = req.body.createLastName;
-    
-    const queryString = "INSERT INTO users (first_name, last_name) VALUES (?, ?)";
-    getConnection().query(queryString, [firstName, lastName], (err, results, fields) => { 
-        if (err) {
-            console.log("Failed to insert new user: " + err);
-            res.sendStatus(500);
-            return
-        }
-        console.log("Inserted a new user with id: ", results.insertId);
-        res.end();
-    }); 
-});
-
-app.get('/user/:id', (req, res) => {
-
-    const userId = req.params.id;
-    const connection = getConnection();
-    connection.query("SELECT * FROM users WHERE id = ?", [userId], (err, rows, fields) => {
-        if (err) {
-            console.log("Failed to query to users: " + err);
-            res.sendStatus(500);
-            return;
-        }
-
-        /* For custom formating for the json.
-        const users = rows.map((row) => {
-            return {firstName: row.first_name, lastName: row.last_name}
-        }); */
-
-        res.json(rows);
-    });
-    //res.end();
-});
-
 app.get("/", (req, res) => {
     console.log("Response to route ");
     res.send("Hello")
-});
-
-app.get("/users", (req, res) => {
-
-    const connection = getConnection();
-    connection.query("SELECT * FROM users", (err, rows, fields) => {
-        if (err) {
-            console.log("Failed to query for users: " + err);
-            res.sendStatus(500)
-            return
-        }
-        res.json(rows);
-    });
 });
 
 var port = process.env.PORT || 3003;
---
+++ NodeJS/MySQLDB/routes/user.js
@@ -0,0 +1,73 @@
+// This file contains all user related routes.
+const express = require('express');
+const mysql = require('mysql');
+const router = express.Router();
+
+const pool = mysql.createPool({
+    connectionLimit: 10,
+    host: 'localhost',
+    user: 'root',
+    database: 'MySQL-API'
+});
+
+function getConnection () {
+    return pool;
+}
+
+router.post('/user_create', (req, res) => {
+
+    const firstName = req.body.createFirstName;
+    const lastName = req.body.createLastName;
+    
+    const queryString = "INSERT INTO users (first_name, last_name) VALUES (?, ?)";
+    getConnection().query(queryString, [firstName, lastName], (err, results, fields) => { 
+        if (err) {
+            console.log("Failed to insert new user: " + err);
+            res.sendStatus(500);
+            return
+        }
+        console.log("Inserted a new user with id: ", results.insertId);
+        res.end();
+    }); 
+});
+
+router.get("/users", (req, res) => {
+
+    const connection = getConnection();
+    connection.query("SELECT * FROM users", (err, rows, fields) => {
+        if (err) {
+            console.log("Failed to query for users: " + err);
+            res.sendStatus(500)
+            return
+        }
+        res.json(rows);
+    });
+});
+
+router.get('/user/:id', (req, res) => {
+
+    const userId = req.params.id;
+    const connection = getConnection();
+    connection.query("SELECT * FROM users WHERE id = ?", [userId], (err, rows, fields) => {
+        if (err) {
+            console.log("Failed to query to users: " + err);
+            res.sendStatus(500);
+            return;
+        }
+
+        /* For custom formating for the json.
+        const users = rows.map((row) => {
+            return {firstName: row.first_name, lastName: row.last_name}
+        }); */
+
+        res.json(rows);
+    });
+    //res.end();
+});
+
+router.get('/messages', (req, res) => {
+    console.log("hi, I'm a message.");
+    res.end();
+});
+
+module.exports = router;
