91229ead60b1dd011f39c7e06abcf54cc041976f
LuanFrz <luan.frizzi@iv2.com.br>
2019-12-17 17:28:36 -0300

    17/12

---
+++ AngularNode/meh/src/app/app.component.ts
@@ -0,0 +1,16 @@
+import { Component } from '@angular/core';
+import { FormBuilder, FormGroup } from '@angular/forms';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+
+  registerForm: FormGroup;
+
+  constructor(private formBuilder: FormBuilder) {}
+
+  title = 'meh';
+}
