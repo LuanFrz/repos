7328cb87dd905ddc03b2680a65d0680e08b80a80
LuanFrz <luan.frizzi@iv2.com.br>
2019-12-17 10:59:01 -0300

    17/12

---
+++ AngularNode/Training/src/app/app-routing.module.ts
@@ -0,0 +1,16 @@
+import { ProductDetailsComponent } from './product-details/product-details.component';
+import { ProductListComponent } from './product-list/product-list.component';
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+
+
+const routes: Routes = [
+  { path: '', component: ProductListComponent },
+  { path: 'products/:productId', component: ProductDetailsComponent }
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
